4be0f0565a57f4e61641b1191d8a92ac99a33a86
Om Prakash <omiiieee15@gmail.com>
2024-06-30 17:35:55 +0530

    api changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors(
   {
-  origin: ["https://task-management-frontend-orpin.vercel.app/"],
+  origin: ["https://task-management-frontend-orpin.vercel.app"],
   methods:
   ["POST", "GET"],
   credentials: true
